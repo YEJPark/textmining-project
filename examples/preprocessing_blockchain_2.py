d90d36d7fb3ba3a3519e56f581855934acd66782
YEJPark <109940982+YEJPark@users.noreply.github.com>
2022-08-08 01:43:24 +0900

    Update preprocessing_blockchain_2.py

--- examples/preprocessing_blockchain_2.py
+++ examples/preprocessing_blockchain_2.py
@@ -2,7 +2,7 @@ import pandas as pd
 import treform as ptm
 from sklearn.feature_extraction.text import CountVectorizer
 
-df = pd.read_csv('C:/Users/yejin/PycharmProjects/lec-text-mining/sample_data/papers_new.csv').fillna("")
+df = pd.read_csv('C:/Users/yejin/PycharmProjects/topic-forecasting-main/data-preprocessing/input_data/patents_new.csv').fillna("")
 df['text'] = df['text'].str.lower()
 corpus = df.iloc[:,1]
 
@@ -19,18 +19,18 @@ result = pipeline.processCorpus(corpus)
 df['text'] = result # 이렇게 하면 문장단위로 list 나옴 ex. [[ , , , ], [ , , , ], [ , , , ], ....]
 
 
-# # document단위로 list통합하고 싶으면 아래 코드 추가 ex. [ , , , , ....]
-# for i in range(len(df['text'])):
-#     df['text'][i] = sum(df['text'][i], [])
+# document단위로 list통합하고 싶으면 아래 코드 추가 ex. [ , , , , ....]
+for i in range(len(df['text'])):
+    df['text'][i] = sum(df['text'][i], [])
 # print(df['text'])
 
 
 import pickle
-with open('C:/Users/yejin/PycharmProjects/lec-text-mining/sample_data/pre_papers.pkl', "wb") as file:
+with open('C:/Users/yejin/PycharmProjects/topic-forecasting-main/data-preprocessing/output_data/pre_patents.pkl', "wb") as file:
     pickle.dump(df, file)
 
 # 어떻게 생겼나 확인
-df1 = pd.read_pickle('C:/Users/yejin/PycharmProjects/lec-text-mining/sample_data/pre_papers.pkl')
+df1 = pd.read_pickle('C:/Users/yejin/PycharmProjects/topic-forecasting-main/data-preprocessing/output_data/pre_patents.pkl')
 #print(df1)
 print(df1.head(40))
 
